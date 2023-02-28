7ebfd4245c56e5be517b77f66a9baba7e1c6ff06
mar-castor <koenig.julian.00@googlemail.com>
2023-02-28 10:36:42 +0100

    insertOne and insertMany

--- app/helper/DB_Connector.js
+++ app/helper/DB_Connector.js
@@ -10,4 +10,24 @@ module.exports = class db_Connector{
         return fetch(`${this.host}/api/collections/${collectionName}/records`)
             .then(r => r.json())
     }
+
+    insertOne(collectionName, entry){
+        //TODO: Validate entry depending on collection
+        return fetch(`${this.host}/api/collections/${collectionName}/records`, {
+            method: 'post',
+            body: JSON.stringify(entry),
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+
+    inserMany(collectionName, entries){
+        if (Array.isArray(entries) && entries.length >= 1){
+            entries.forEach(entry => {
+                this.insertOne(collectionName, entry)
+            });
+        }
+        else{
+            throw new Error("no Entries to insert found")
+        }
+    }
 }
