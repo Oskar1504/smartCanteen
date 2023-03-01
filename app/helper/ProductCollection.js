59f354dd59baea46c111fcb093f9607ae0d0e2fe
mar-castor <koenig.julian.00@googlemail.com>
2023-03-01 12:16:18 +0100

    correct Price in ordee

--- app/helper/ProductCollection.js
+++ app/helper/ProductCollection.js
@@ -3,7 +3,32 @@ module.exports = class productCollection{
     constructor(db, maxCacheMinutes = 1){
         this.db = db
         this.maxCacheMinutes = maxCacheMinutes * 60 * 1000
-        this.productCollection = fetchProducts()
+        this.productCollection = loadProducts()
         this.productsLastFetched = new Date().getTime()
+    }
+
+
+    loadProducts(){
+        this.db.getCollection("products")
+            .then(d => {
+                this.productsLastFetched = new Date().getTime()
+                this.products = Object.fromEntries(d.items.map(e => [e.id,e]))
+        })
+    }
+
+    getUser(userId){
+        this.checkCache()
+        //TODO check existance
+        return this.users[userId]
+    }
+
+    checkCache(){
+        let time = new Date().getTime()
+        if(time - this.userLastFetched >= this.maxCacheMinutes){
+            console.log("[USERCOLLECTION] reloading user data")
+            this.loadProducts()
+            return false
+        }
+        return true
     }
 }
